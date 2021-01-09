148f618eaa9684201aac1efd244af492c8627848
Doxteur <Doxteurn@gmail.com>
2021-01-09 17:10:30 +0100

    Test

---
+++ src/Jimmy.java
@@ -0,0 +1,55 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Jimmy implements ActionListener {
+	int click = 0;
+	JFrame frame;
+	JLabel label;
+	JPanel panel;
+	public Jimmy() {
+
+		// Setup des objects
+		frame = new JFrame();
+		JButton button = new JButton("Click me");
+		panel = new JPanel();
+		label = new JLabel("Number of clicks: 0");
+
+		// Setup Other
+		button.addActionListener(this);
+
+		// Setup du panel
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+		panel.setLayout(new GridLayout(0, 1));
+		panel.add(button);
+		panel.add(label);
+		
+		// Setup de la frame
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Our GUI");
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+
+		new Jimmy();
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		click +=1;
+		label.setText("Number of click : " + click);
+		
+	}
+
+}
---
+++ src/chien.java
@@ -0,0 +1,15 @@
+
+public class chien {
+	String name;
+	int age;
+	
+	public chien() {
+		
+	}
+	public void setName(String setName) {
+		name = setName;
+	}
+	public void setAge(int setAge) {
+		age = setAge;
+	}
+}
